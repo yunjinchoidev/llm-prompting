03ba9e4b480efb65e1e50d7b58f0814f5b3958a6
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-08-15 03:04:08 +0900

    [Fix] 파싱 에러 수정

--- fastapi_server.py
+++ fastapi_server.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from langchain.agents import AgentType
+from langchain.agents.agent_toolkits import create_python_agent
+from langchain.chat_models import ChatOpenAI
+from langchain.tools.python.tool import PythonREPLTool
+from langchain import PromptTemplate, LLMChain
+from load_dotenv import load_dotenv
 
+load_dotenv()
 app = FastAPI()
 
 
@@ -20,6 +28,38 @@ def greet():
             non dictum mauris. Nulla at tellus sagittis, viverra est a, bibendum metus.
 """
     }
+
+
+from pydantic import BaseModel
+
+
+class SummaryRequest(BaseModel):
+    information: str
+
+
+@app.post("/summarize/")
+def summarize(request: SummaryRequest) -> dict:
+    information = request.information
+
+    # 요약 로직을 수행하는 부분입니다.
+    # 여기서는 'main' 함수와 유사하게 작성하실 수 있습니다.
+    summay_template = """
+        뉴스 정보를 주겠다. {information}
+        뉴스의 핵심만 두 줄로 요약하세요.
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"],
+        template=summay_template,
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k-0613")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    summary = chain.run(information=information)
+
+    return {"summary": summary}
 
 
 if __name__ == "__main__":
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,21 @@ import requests
 
 url = st.text_input("URL을 입력하세요")
 
+summary_text = st.text_area("요약할 텍스트를 입력하세요")
+
 if url:
     response = requests.get("http://localhost:8000/greet")
     if response.status_code == 200:
         st.write(response.json()["message"])
+    else:
+        st.write("오류 발생:", response.status_code)
+
+
+if summary_text:
+    response = requests.post(
+        "http://localhost:8000/summarize/", json={"information": summary_text}
+    )
+    if response.status_code == 200:
+        st.write(response.json()["summary"])
     else:
         st.write("오류 발생:", response.status_code)
