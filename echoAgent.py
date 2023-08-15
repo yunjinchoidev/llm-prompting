69f57d8899db6b64673a7d604b9df7d023298827
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-08-15 23:27:00 +0900

    [Fix] 메모리 기반 대화 코드 추가

--- echoAgent.py
+++ echoAgent.py
@@ -43,6 +43,9 @@ if "user_prompt_history" not in st.session_state:
 if "chat_answers_history" not in st.session_state:
     st.session_state["chat_answers_history"] = []
 
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
 
 def create_sources_string(source_urls: Set[str]) -> str:
     if not source_urls:
@@ -57,17 +60,21 @@ def create_sources_string(source_urls: Set[str]) -> str:
 
 if prompt:
     with st.spinner("Generating response.."):
-        generated_response = run_llm(query=prompt)
+        generated_response = run_llm(
+            query=prompt, chat_history=st.session_state["chat_history"]
+        )
         sources = set(
             [doc.metadata["source"] for doc in generated_response["source_documents"]]
         )
 
         formatted_response = (
-            f"{generated_response['result']} \n\n {create_sources_string(sources)}"
+            f"{generated_response['answer']} \n\n {create_sources_string(sources)}"
         )
 
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["chat_answers_history"].append(formatted_response)
+        st.session_state["chat_history"].append((prompt, generated_response["answer"]))
+
 
 if st.session_state["chat_answers_history"]:
     for generated_response, user_query in zip(
--- talk2docs.py
+++ talk2docs.py
@@ -1,8 +1,8 @@
-from typing import Any
+from typing import Any, List, Dict
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
-from langchain.chains import RetrievalQA
+from langchain.chains import RetrievalQA, ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 import pinecone
 import os
@@ -16,18 +16,25 @@ pinecone.init(
 )
 
 
-def run_llm(query: str) -> Any:
-    embbings = OpenAIEmbeddings()
-    docsearch = Pinecone.from_existing_index(index_name="news", embedding=embbings)
+def run_llm(query: str, chat_history: List[Dict[str, Any]]) -> Any:
+    embeddings = OpenAIEmbeddings()
+    docsearch = Pinecone.from_existing_index(index_name="news", embedding=embeddings)
     chat = ChatOpenAI(verbose=True, temperature=0)
-    qa = RetrievalQA.from_chain_type(
+    # qa = RetrievalQA.from_chain_type(
+    #     llm=chat,
+    #     chain_type="stuff",
+    #     retriever=docsearch.as_retriever(),
+    #     return_source_documents=True,
+    # )
+
+    # 메모리
+    qa = ConversationalRetrievalChain.from_llm(
         llm=chat,
-        chain_type="stuff",
         retriever=docsearch.as_retriever(),
         return_source_documents=True,
     )
 
-    return qa({"query": query})
+    return qa({"question": query, "chat_history": chat_history})
 
 
 if __name__ == "__main__":
