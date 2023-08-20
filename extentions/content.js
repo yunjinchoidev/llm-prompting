6e9cf13ade3e65a1dfbdf89710dd04c5402b2609
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-08-20 14:57:43 +0900

    [Fix] chrome extion 형식

--- extentions/content.js
+++ extentions/content.js
@@ -1,17 +1,26 @@
-const chatbotHTML = `
-  <div id="chatbot" class="chatbot">
-    <img src="chatbot_icon.png" id="chatbot-icon" class="chatbot-icon" alt="Chatbot">
-    <div class="chat-window" style="display:none;">
-      <div class="messages"></div>
-      <input type="text" class="user-input" placeholder="메시지 입력"/>
-    </div>
-  </div>`;
+var chatBox = document.createElement('div');
+chatBox.id = 'chat-box';
+document.body.appendChild(chatBox);
 
-document.body.insertAdjacentHTML('beforeend', chatbotHTML);
+var chatLog = document.createElement('textarea');
+chatLog.id = 'chat-log';
+chatLog.readOnly = true;
+chatBox.appendChild(chatLog);
 
-const chatbotIcon = document.getElementById('chatbot-icon');
-const chatWindow = document.querySelector('.chat-window');
+var chatInput = document.createElement('input');
+chatInput.id = 'chat-input';
+chatInput.type = 'text';
+chatBox.appendChild(chatInput);
 
-chatbotIcon.addEventListener('click', () => {
-  chatWindow.style.display = chatWindow.style.display === 'none' ? 'block' : 'none';
-});
+var sendButton = document.createElement('button');
+sendButton.id = 'send-button';
+sendButton.textContent = '보내기';
+sendButton.onclick = sendMessage;
+chatBox.appendChild(sendButton);
+
+function sendMessage() {
+  chatLog.value += "나: " + chatInput.value + "\n";
+  // 챗봇 응답 로직
+  chatLog.value += "챗봇: 안녕하세요!\n"; // 예시 응답
+  chatInput.value = '';
+}
